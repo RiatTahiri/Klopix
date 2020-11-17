d15e443098de3d5643c3c47a7f03384942be4132
Rijad <badvidgamer@gmail.com>
2020-11-17 11:55:28 +0100

    Changes

--- server/db.js
+++ server/db.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 
-const connect = () => {
+module.exports.connectDatabase = () => {
   try {
     mongoose.connect(process.env.MONGO_KEY, { useNewUrlParser: true }, () => {
       console.log("Connected To Database");
     });
   } catch (err) {
     console.log(`Can't Connect To Database ${err}`);
+    process.exit(1);
   }
 };
-
-module.exports = connect;
--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const UserRoutes = require("./routes/User_Routes");
 
 const db = require("./db.js");
 
 const morgan = require("morgan");
-// db.connect;
+db.connectDatabase();
 
 app.use(morgan("dev"));
 app.use("/", UserRoutes); // User Routes
